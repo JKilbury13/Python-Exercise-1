1f6bc7d3839c08d8d8ab7577c243bd7760249f8b
JKilbury13 <jkilbury13@gmail.com>
2017-08-30 09:17:15 -0700

    Add files via upload

---
+++ PythonInADay_Exercise1.py
@@ -0,0 +1,21 @@
+Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 20:42:59) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print 'this stuff rocks'
+this stuff rocks
+>>> x = 10
+>>> print x
+10
+>>> y = 2
+>>> print (x+y)
+12
+>>> print (x-y)
+8
+>>> print (x*y)
+20
+>>> print (x/y)
+5
+>>> some_int_variable = 2
+>>> some_float_variable = 1.24
+>>> some_boolean_variable = True
+>>> some_string_value = "awesome text here"
+>>> 
